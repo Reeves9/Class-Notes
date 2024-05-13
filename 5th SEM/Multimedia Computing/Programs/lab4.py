77d4474f1f3a9289fc1dda9fe5a8388c70b1b720
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2024-05-13 07:00:57 +0545

    lab files

---
+++ 5th SEM/Multimedia Computing/Programs/lab4.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+grey_image = Image.open("Lenna_grey.jpg")
+grey_array = np.array(grey_image)
+
+threshold = 128
+binary_array = np.where(grey_array > threshold, 0, 255)
+
+binary_image = Image.fromarray(binary_array.astype(np.uint8))
+
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(grey_image, cmap='gray')
+plt.title('Grayscale Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(binary_image, cmap='binary')
+plt.title('Binary Image')
+plt.axis('off')
+
+plt.show()
