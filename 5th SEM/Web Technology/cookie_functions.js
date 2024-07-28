a9227ec54fd32201d36d79765e9dcc9c4869c418
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2024-07-28 08:01:04 +0545

    cookie bullshit

---
+++ 5th SEM/Web Technology/cookie_functions.js
@@ -0,0 +1,22 @@
+funtion setCookie(cname,cvalue,exdays){
+	const d = new date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	let expires = "expires="+d.toUTCString();
+	document.cookie = cname+"="+cvalue+";"+expires+"; path=/";
+}
+
+function getCookie(cname){
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let cs = decodedCookie.split(';');
+	for(let i = 0; i < ca.lenght; i++){
+		let c = ca[i];
+		while(c.charAt(0) == ' '){
+			c = c.substring(1);
+		}
+		if(c.indexOf(name) == 0){
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
