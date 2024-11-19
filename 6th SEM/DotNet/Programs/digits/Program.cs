a95122bd654491bc70c715534fa37032fed5867d
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2024-11-19 07:17:26 +0545

    class problems

---
+++ 6th SEM/DotNet/Programs/digits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace digits{
+    class Program{
+        static int factorial(int num){
+            int result = 1;
+            for (int i=1;i<=num;i++){
+                result *= i;
+            }
+            return result;
+        }
+        static void Main(){
+            int num,proxy,rem,fac=0;
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            proxy = num;
+            while (num != 0){
+                rem = num % 10;
+                fac += factorial(rem);
+                num /= 10;
+            }
+            if (fac == proxy){
+                Console.WriteLine("Strong number");
+            }else{
+                Console.WriteLine("Not strong number");
+            }
+            int sum=0;
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0){
+                rem = num % 10;
+                sum += rem;
+                num /= 10;
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
---
+++ 6th SEM/DotNet/Programs/patterns/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1;i<=10;i++){
+                Console.Write($"{i*i} ");
+            }
+            Console.WriteLine();
+            for (int i=1;i<=20;i++){
+                Console.Write($"{i*i*i} ");
+            }
+            Console.WriteLine();
+            for (int i=1;i<=20;i++){
+                Console.Write($"{(i*i)+(i*i*i)} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+            
